f89d4ec5dd155e9b95964f04fa2d4324cfd1c372
Amilcar Cornier <amilcarcornier@Amilcars-Mac-mini.local>
2024-09-19 22:47:18 -0400

    python loop assignment

---
+++ Module 2 - python lists/Python List Transformation.py
@@ -0,0 +1,8 @@
+
+grades = [85,90,78,88,76,95,89,80,72,93]
+descending_order = grades
+descending_order = grades.sort(reverse = True)
+print(f"The Descending order is: {grades}")
+average_grade = sum(grades)
+calculate_average = average_grade / len(grades)
+print(f"The average is: {calculate_average}")
